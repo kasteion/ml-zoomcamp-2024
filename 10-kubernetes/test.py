e97350d76c1b2dcb4ad9a7dbcddd1034ea23d874
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2024-12-20 22:48:18 -0500

    (kubernetes) deploying to eks

--- 10-kubernetes/test.py
+++ 10-kubernetes/test.py
@@ -1,6 +1,9 @@
 import requests
 
+# With Port Forwarding
 # url = 'http://localhost:9696/predict'
+
+# From k8s cluster
 url = 'http://localhost:8080/predict'
 
 data = { 'url': 'http://bit.ly/mlbookcamp-pants' }
